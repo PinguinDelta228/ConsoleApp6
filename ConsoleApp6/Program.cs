3968194a54a2effa8231df106aad8fb4cefacd10
STUDENT <STUDENT@K-405-1>
2024-01-26 09:08:55 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+ namespace Consoleapp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int a1 = int.Parse(a);
+            if (a1 < 18)
+            {
+                Console.WriteLine("Иди отсюда лялька");
+            }
+            else
+            {
+                Console.WriteLine("Здраствуй небо в облаках");
+            }
+            
+        }
+    }
+}
